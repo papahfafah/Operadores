95823bfaffc6e72d43e128513b068d80a0568c10
papahfafah <128807287+papahfafah@users.noreply.github.com>
2024-05-02 12:05:22 -0300

    Add files via upload

---
+++ operações.js
@@ -0,0 +1,20 @@
+
+const primeiroNumber = 1
+const segundNumber = 6
+
+const operaçãoMatematica = primeiroNumber + segundNumber;
+
+const primeiraInformacao = 'quem é?'
+const segundaInformacao = ' Japonego';
+
+const operacaoTexto = primeiraInformacao + segundaInformacao;
+
+console.log (operaçãoMatematica)
+console.log (operacaoTexto)
+
+const informacaoum = 'de onde ele é?'
+const informacaodois = ' ele é da JapÁfrica';
+
+const operacaoTexto2 = informacaoum + informacaodois;
+
+console.log (operacaoTexto2)
